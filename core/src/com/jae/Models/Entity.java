6577f882e82e1ea50c3f6a647252ecfc8a4f5c6d
alex aka turri <torrents.alex1@gmail.com>
2017-10-24 19:03:15 +0200

    methods to add velocity to an entity

--- core/src/com/jae/Models/Entity.java
+++ core/src/com/jae/Models/Entity.java
@@ -1,9 +1,12 @@
 package com.jae.Models;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+
+import java.util.Vector;
 
 /**
  * Created by Alex Torrents (AKA Turri) on 22-Oct-17.
@@ -13,14 +16,21 @@ public class Entity {
 
     protected Texture texture;
     protected Rectangle rectangle;
+    protected Vector2 velocity;
 
     public Entity() {
         rectangle = new Rectangle();
+        init();
     }
 
     public Entity(String path) {
         texture = new Texture(path);
+        init();
+    }
+
+    private void init() {
         rectangle = new Rectangle();
+        velocity = new Vector2(0, 0);
     }
 
     public void setPosition(Vector2 pos) {
@@ -35,12 +45,15 @@ public class Entity {
         rectangle.setSize(x, y);
     }
 
-    public void dispose() {
-        if (texture != null) {
-            texture.dispose();
-        }
-    }
+    public void setVelocity(int x, int y) { velocity.set(x, y);}
+    public void setVelocity(Vector2 vel) { velocity.set(vel.x, vel.y);}
 
+    public void addVelocity(int x, int y) { velocity.add(x, y);}
+    public void addVelocity(Vector2 vel) { velocity.add(vel.x, vel.y);}
+
+    /**
+     *  Plus the current position.
+     * */
     public void addPosition(float x, int y) {
         rectangle.x += x;
         rectangle.y += y;
@@ -50,10 +63,16 @@ public class Entity {
         batch.draw(texture, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
     }
 
-    public void update() {}
+    public void update() {
+        if (velocity != new Vector2(0, 0)) {
+            rectangle.x += velocity.x * Gdx.graphics.getDeltaTime();
+            rectangle.y += velocity.y * Gdx.graphics.getDeltaTime();
+        }
+    }
 
-    /* GETTERS */
-
+    /**
+     * @return the width and height
+     * */
     public Vector2 getSize() {
         return new Vector2(rectangle.getWidth(), rectangle.getHeight());
     }
@@ -62,6 +81,10 @@ public class Entity {
         return rectangle;
     }
 
+    /**
+     * @return if a point is inside the object,
+     *          not use for collision between 2 objects
+     * */
     public boolean contains(float x, float y) {
         if (rectangle.contains(x, y)) {
             return true;
@@ -69,10 +92,9 @@ public class Entity {
         return false;
     }
 
-    public boolean contains(Entity e1) {
-        if (rectangle.contains(e1.getRectangle())) {
-            return true;
+    public void dispose() {
+        if (texture != null) {
+            texture.dispose();
         }
-        return false;
     }
 }
