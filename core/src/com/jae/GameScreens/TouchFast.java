52a4984e959c95c93e39810d4ab2113c3ed0b8aa
turri <torrents.alex1@gmail.com>
2017-11-05 11:48:48 +0100

    testing with a new GameScreen class for better logic in games

--- core/src/com/jae/GameScreens/TouchFast.java
+++ core/src/com/jae/GameScreens/TouchFast.java
@@ -2,33 +2,32 @@ package com.jae.GameScreens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.jae.JAEScreen;
+import com.jae.Models.GameScreen;
+import com.jae.Models.JAEScreen;
 import com.jae.Models.Entity;
 
 /**
  * Created by Alex Torrents (AKA Turri) on 22-Oct-17.
  */
 
-public class TouchFast extends JAEScreen {
-    Entity background;
+public class TouchFast extends GameScreen {
 
-    Entity playButton;
+    private Entity playButton;
+
+    private ParticleEffect startExplosion;
 
     public TouchFast(Game game) {
         super(game);
         init();
     }
 
-
     @Override
     public void update(float delta) {
-        camera.update();
+        super.update(delta);
+
 
         if (Gdx.input.isTouched()) {
             Vector3 touchpos = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0.0f));
@@ -40,7 +39,7 @@ public class TouchFast extends JAEScreen {
 
     @Override
     public void draw(float delta) {
-        background.draw(batch);
+        super.draw(delta);
         playButton.draw(batch);
     }
 
@@ -52,13 +51,9 @@ public class TouchFast extends JAEScreen {
     private void init() {
         done = false;
 
-        background = new Entity("airadventurelevel2.png");
-        background.setPosition(new Vector2(0,0));
-        background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        playButton = new Entity("b_round_g.png");
-        playButton.setPosition(new Vector2(Gdx.graphics.getWidth()/2.5f, Gdx.graphics.getHeight()/2.5f));
-        playButton.setSize(new Vector2( Gdx.graphics.getHeight()/5, Gdx.graphics.getHeight()/5));
+        backgroundGame = new Entity("airadventurelevel2.png");
+        backgroundGame.setPosition(new Vector2(0,0));
+        backgroundGame.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         Gdx.app.debug("DontBuyMe","init Touch fast game");
     }
@@ -66,5 +61,20 @@ public class TouchFast extends JAEScreen {
     @Override
     public void dispose() {
         super.dispose();
+    }
+
+    @Override
+    public void drawGame(float delta) {
+
+    }
+
+    @Override
+    public void drawWin(float delta) {
+
+    }
+
+    @Override
+    public void drawFail(float delta) {
+
     }
 }
--- core/src/com/jae/Utils/GameLogic.java
+++ core/src/com/jae/Utils/GameLogic.java
@@ -5,4 +5,6 @@ package com.jae.Utils;
  */
 
 public interface GameLogic {
+    void finishGame();
+    void startGame();
 }
