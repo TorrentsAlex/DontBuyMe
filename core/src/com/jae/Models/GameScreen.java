ef251c03a9ae63dbb880c54063214ef3629b08e5
alex aka turri <torrents.alex1@gmail.com>
2017-11-05 11:11:31 +0100

    new assets

---
+++ core/src/com/jae/Models/GameScreen.java
@@ -0,0 +1,8 @@
+package com.jae.Models;
+
+/**
+ * Created by Alex Torrents (aka Turri) on 03-Nov-17.
+ */
+
+public class GameScreen extends JAEScreen {
+}
---
+++ core/src/com/jae/Utils/GameLogic.java
@@ -0,0 +1,8 @@
+package com.jae.Utils;
+
+/**
+ * Created by Alex Torrents (aka Turri) on 02-Nov-17.
+ */
+
+public interface GameLogic {
+}
